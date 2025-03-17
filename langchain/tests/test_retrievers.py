ed1686526c85f6baa9a073e9f9396d1115fdbc2e
cbornet <cbornet@hotmail.com>
2025-03-17 18:52:15 +0100

    Add tests in CI

--- langchain/tests/test_retrievers.py
+++ langchain/tests/test_retrievers.py
@@ -1,28 +1,24 @@
 import json
 import logging
 import os
+import time
 from collections.abc import Iterator
-from dataclasses import dataclass
 from pathlib import Path
+from typing import Literal
 
 import pytest
+import urllib3
 import vectorize_client as v
+from vectorize_client import ApiClient
 
 from langchain_vectorize.retrievers import VectorizeRetriever
-
-
-@dataclass
-class TestContext:
-    api_client: v.ApiClient
-    api_token: str
-    org_id: str
 
 
 @pytest.fixture(scope="session")
 def api_token() -> str:
     token = os.getenv("VECTORIZE_TOKEN")
     if not token:
-        msg = "Please set VECTORIZE_TOKEN environment variable"
+        msg = "Please set the VECTORIZE_TOKEN environment variable"
         raise ValueError(msg)
     return token
 
@@ -31,21 +27,29 @@ def api_token() -> str:
 def org_id() -> str:
     org = os.getenv("VECTORIZE_ORG")
     if not org:
-        msg = "Please set VECTORIZE_ORG environment variable"
+        msg = "Please set the VECTORIZE_ORG environment variable"
         raise ValueError(msg)
     return org
 
 
 @pytest.fixture(scope="session")
-def api_client(api_token: str) -> Iterator[TestContext]:
+def environment() -> Literal["prod", "dev", "local", "staging"]:
     env = os.getenv("VECTORIZE_ENV", "prod")
+    if env not in ["prod", "dev", "local", "staging"]:
+        msg = "Invalid VECTORIZE_ENV environment variable."
+        raise ValueError(msg)
+    return env
+
+
+@pytest.fixture(scope="session")
+def api_client(api_token: str, environment: str) -> Iterator[ApiClient]:
     header_name = None
     header_value = None
-    if env == "prod":
+    if environment == "prod":
         host = "https://api.vectorize.io/v1"
-    elif env == "dev":
+    elif environment == "dev":
         host = "https://api-dev.vectorize.io/v1"
-    elif env == "local":
+    elif environment == "local":
         host = "http://localhost:3000/api"
         header_name = "x-lambda-api-key"
         header_value = api_token
@@ -86,8 +90,6 @@ def pipeline_id(api_client: v.ApiClient, org_id: str) -> Iterator[str]:
             metadata=json.dumps({"created-from-api": True}),
         ),
     )
-
-    import urllib3
 
     http = urllib3.PoolManager()
     this_dir = Path(__file__).parent
@@ -137,7 +139,9 @@ def pipeline_id(api_client: v.ApiClient, org_id: str) -> Iterator[str]:
                 config={},
             ),
             ai_platform=v.AIPlatformSchema(
-                id=builtin_ai_platform, type=v.AIPlatformType.VECTORIZE, config=v.AIPlatformConfigSchema()
+                id=builtin_ai_platform,
+                type=v.AIPlatformType.VECTORIZE,
+                config=v.AIPlatformConfigSchema(),
             ),
             pipeline_name="Test pipeline",
             schedule=v.ScheduleSchema(type=v.ScheduleSchemaType.MANUAL),
@@ -154,20 +158,48 @@ def pipeline_id(api_client: v.ApiClient, org_id: str) -> Iterator[str]:
         logging.exception("Failed to delete pipeline %s", pipeline_id)
 
 
-def test_retrieve_init_args(api_token: str, org_id: str, pipeline_id: str) -> None:
+def test_retrieve_init_args(
+    environment: Literal["prod", "dev", "local", "staging"],
+    api_token: str,
+    org_id: str,
+    pipeline_id: str,
+) -> None:
     retriever = VectorizeRetriever(
-        api_token=api_token, organization=org_id, pipeline_id=pipeline_id, num_results=2
-    )
-    docs = retriever.invoke(input="What are you?")
-    assert len(docs) == 2
-
-
-def test_retrieve_invoke_args(api_token: str, org_id: str, pipeline_id: str) -> None:
-    retriever = VectorizeRetriever(api_token=api_token)
-    docs = retriever.invoke(
-        input="What are you?",
+        environment=environment,
+        api_token=api_token,
         organization=org_id,
         pipeline_id=pipeline_id,
         num_results=2,
     )
-    assert len(docs) == 2
+    start = time.time()
+    while True:
+        docs = retriever.invoke(input="What are you?")
+        if len(docs) == 2:
+            break
+        if time.time() - start > 180:
+            msg = "Docs not retrieved in time"
+            raise RuntimeError(msg)
+        time.sleep(1)
+
+
+def test_retrieve_invoke_args(
+    environment: Literal["prod", "dev", "local", "staging"],
+    api_token: str,
+    org_id: str,
+    pipeline_id: str,
+) -> None:
+    retriever = VectorizeRetriever(environment=environment, api_token=api_token)
+    start = time.time()
+    while True:
+        docs = retriever.invoke(
+            input="What are you?",
+            organization=org_id,
+            pipeline_id=pipeline_id,
+            num_results=2,
+        )
+        if len(docs) == 2:
+            break
+        if time.time() - start > 180:
+            msg = "Docs not retrieved in time"
+            raise RuntimeError(msg)
+        time.sleep(1)
