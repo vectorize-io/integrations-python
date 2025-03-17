3633eda2cee84541ca0d749bdfa3051f586a8dfc
cbornet <cbornet@hotmail.com>
2025-03-17 17:00:04 +0100

    Add Langchain retriever

---
+++ langchain/langchain_vectorize/retrievers.py
@@ -0,0 +1,145 @@
+"""Vectorize LangChain retrievers."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any, Optional
+
+import vectorize_client
+from langchain_core.documents import Document
+from langchain_core.retrievers import BaseRetriever
+from typing_extensions import override
+from vectorize_client import (
+    ApiClient,
+    Configuration,
+    PipelinesApi,
+    RetrieveDocumentsRequest,
+)
+
+if TYPE_CHECKING:
+    from langchain_core.callbacks import CallbackManagerForRetrieverRun
+    from langchain_core.runnables import RunnableConfig
+
+_METADATA_FIELDS = {
+    "relevancy",
+    "chunk_id",
+    "total_chunks",
+    "origin",
+    "origin_id",
+    "similarity",
+    "source",
+    "unique_source",
+    "source_display_name",
+    "pipeline_id",
+    "org_id",
+}
+_NOT_SET = object()
+
+
+class VectorizeRetriever(BaseRetriever):
+    """Vectorize retriever."""
+
+    api_token: str
+    """The Vectorize API token."""
+    organization: Optional[str] = None  # noqa: UP007
+    """The Vectorize organization ID."""
+    pipeline_id: Optional[str] = None  # noqa: UP007
+    """The Vectorize pipeline ID."""
+    num_results: int = 5
+    """The number of documents to return."""
+    rerank: bool = False
+    """Whether to rerank the results."""
+    metadata_filters: list[dict[str, Any]] = []
+    """The metadata filters to apply when retrieving the documents."""
+
+    _pipelines: PipelinesApi | None = None
+
+    @override
+    def model_post_init(self, /, context: Any) -> None:
+        api = ApiClient(Configuration(access_token=self.api_token))
+        self._pipelines = PipelinesApi(api)
+
+    @staticmethod
+    def _convert_document(document: vectorize_client.models.Document) -> Document:
+        metadata = {field: getattr(document, field) for field in _METADATA_FIELDS}
+        return Document(id=document.id, page_content=document.text, metadata=metadata)
+
+    @override
+    def _get_relevant_documents(
+        self,
+        query: str,
+        *,
+        run_manager: CallbackManagerForRetrieverRun,
+        organization: str | None = None,
+        pipeline_id: str | None = None,
+        num_results: int | None = None,
+        rerank: bool | None = None,
+        metadata_filters: list[dict[str, Any]] | None = None,
+    ) -> list[Document]:
+        request = RetrieveDocumentsRequest(
+            question=query,
+            num_results=num_results or self.num_results,
+            rerank=rerank or self.rerank,
+            metadata_filters=metadata_filters or self.metadata_filters,
+        )
+        response = self._pipelines.retrieve_documents(
+            organization or self.organization, pipeline_id or self.pipeline_id, request
+        )
+        return [self._convert_document(doc) for doc in response.documents]
+
+    @override
+    def invoke(
+        self,
+        input: str,
+        config: RunnableConfig | None = None,
+        *,
+        organization: str = "",
+        pipeline_id: str = "",
+        num_results: int = _NOT_SET,
+        rerank: bool = _NOT_SET,
+        metadata_filters: list[dict[str, Any]] = _NOT_SET,
+    ) -> list[Document]:
+        """Invoke the retriever to get relevant documents.
+
+        Main entry point for retriever invocations.
+
+        Args:
+            input: The query string.
+            config: Configuration for the retriever. Defaults to None.
+            organization: The organization to retrieve documents from.
+                If set, overrides the organization set at the initialization of the
+                retriever.
+            pipeline_id: The pipeline ID to retrieve documents from.
+                If set, overrides the pipeline ID set at the initialization of the
+                retriever.
+            num_results: The number of results to retrieve.
+                If set, overrides the number of results set at the initialization of
+                the retriever.
+            rerank: Whether to rerank the retrieved documents.
+                If set, overrides the reranking set at the initialization of the
+                retriever.
+            metadata_filters: The metadata filters to apply when retrieving documents.
+                If set, overrides the metadata filters set at the initialization of the
+                retriever.
+
+        Returns:
+            List of relevant documents.
+
+        Examples:
+
+            .. code-block:: python
+
+                retriever.invoke("query")
+        """
+        kwargs = {}
+        if organization:
+            kwargs["organization"] = organization
+        if pipeline_id:
+            kwargs["pipeline_id"] = pipeline_id
+        if num_results is not _NOT_SET:
+            kwargs["num_results"] = num_results
+        if rerank is not _NOT_SET:
+            kwargs["rerank"] = rerank
+        if metadata_filters is not _NOT_SET:
+            kwargs["metadata_filters"] = metadata_filters
+
+        return super().invoke(input, config, **kwargs)
---
+++ langchain/tests/test_retrievers.py
@@ -0,0 +1,173 @@
+import json
+import logging
+import os
+from collections.abc import Iterator
+from dataclasses import dataclass
+from pathlib import Path
+
+import pytest
+import vectorize_client as v
+
+from langchain_vectorize.retrievers import VectorizeRetriever
+
+
+@dataclass
+class TestContext:
+    api_client: v.ApiClient
+    api_token: str
+    org_id: str
+
+
+@pytest.fixture(scope="session")
+def api_token() -> str:
+    token = os.getenv("VECTORIZE_TOKEN")
+    if not token:
+        msg = "Please set VECTORIZE_TOKEN environment variable"
+        raise ValueError(msg)
+    return token
+
+
+@pytest.fixture(scope="session")
+def org_id() -> str:
+    org = os.getenv("VECTORIZE_ORG")
+    if not org:
+        msg = "Please set VECTORIZE_ORG environment variable"
+        raise ValueError(msg)
+    return org
+
+
+@pytest.fixture(scope="session")
+def api_client(api_token: str) -> Iterator[TestContext]:
+    env = os.getenv("VECTORIZE_ENV", "prod")
+    header_name = None
+    header_value = None
+    if env == "prod":
+        host = "https://api.vectorize.io/v1"
+    elif env == "dev":
+        host = "https://api-dev.vectorize.io/v1"
+    elif env == "local":
+        host = "http://localhost:3000/api"
+        header_name = "x-lambda-api-key"
+        header_value = api_token
+    else:
+        host = "https://api-staging.vectorize.io/v1"
+
+    with v.ApiClient(
+        v.Configuration(host=host, access_token=api_token, debug=True),
+        header_name,
+        header_value,
+    ) as api:
+        yield api
+
+
+@pytest.fixture(scope="session")
+def pipeline_id(api_client: v.ApiClient, org_id: str) -> Iterator[str]:
+    pipelines = v.PipelinesApi(api_client)
+
+    connectors_api = v.ConnectorsApi(api_client)
+    response = connectors_api.create_source_connector(
+        org_id,
+        [
+            v.CreateSourceConnector(
+                name="from api", type=v.SourceConnectorType.FILE_UPLOAD
+            )
+        ],
+    )
+    source_connector_id = response.connectors[0].id
+    logging.info("Created source connector %s", source_connector_id)
+
+    uploads_api = v.UploadsApi(api_client)
+    upload_response = uploads_api.start_file_upload_to_connector(
+        org_id,
+        source_connector_id,
+        v.StartFileUploadToConnectorRequest(
+            name="research.pdf",
+            content_type="application/pdf",
+            metadata=json.dumps({"created-from-api": True}),
+        ),
+    )
+
+    import urllib3
+
+    http = urllib3.PoolManager()
+    this_dir = Path(__file__).parent
+    file_path = this_dir / "research.pdf"
+
+    with file_path.open("rb") as f:
+        http_response = http.request(
+            "PUT",
+            upload_response.upload_url,
+            body=f,
+            headers={
+                "Content-Type": "application/pdf",
+                "Content-Length": str(file_path.stat().st_size),
+            },
+        )
+    if http_response.status != 200:
+        msg = "Upload failed:"
+        raise ValueError(msg)
+    else:
+        logging.info("Upload successful")
+
+    ai_platforms = connectors_api.get_ai_platform_connectors(org_id)
+    builtin_ai_platform = next(
+        c.id for c in ai_platforms.ai_platform_connectors if c.type == "VECTORIZE"
+    )
+    logging.info("Using AI platform %s", builtin_ai_platform)
+
+    vector_databases = connectors_api.get_destination_connectors(org_id)
+    builtin_vector_db = next(
+        c.id for c in vector_databases.destination_connectors if c.type == "VECTORIZE"
+    )
+    logging.info("Using destination connector %s", builtin_vector_db)
+
+    pipeline_response = pipelines.create_pipeline(
+        org_id,
+        v.PipelineConfigurationSchema(
+            source_connectors=[
+                v.SourceConnectorSchema(
+                    id=source_connector_id,
+                    type=v.SourceConnectorType.FILE_UPLOAD,
+                    config={},
+                )
+            ],
+            destination_connector=v.DestinationConnectorSchema(
+                id=builtin_vector_db,
+                type=v.DestinationConnectorType.VECTORIZE,
+                config={},
+            ),
+            ai_platform=v.AIPlatformSchema(
+                id=builtin_ai_platform, type=v.AIPlatformType.VECTORIZE, config=v.AIPlatformConfigSchema()
+            ),
+            pipeline_name="Test pipeline",
+            schedule=v.ScheduleSchema(type=v.ScheduleSchemaType.MANUAL),
+        ),
+    )
+    pipeline_id = pipeline_response.data.id
+    logging.info("Created pipeline %s", pipeline_id)
+
+    yield pipeline_id
+
+    try:
+        pipelines.delete_pipeline(org_id, pipeline_id)
+    except Exception:
+        logging.exception("Failed to delete pipeline %s", pipeline_id)
+
+
+def test_retrieve_init_args(api_token: str, org_id: str, pipeline_id: str) -> None:
+    retriever = VectorizeRetriever(
+        api_token=api_token, organization=org_id, pipeline_id=pipeline_id, num_results=2
+    )
+    docs = retriever.invoke(input="What are you?")
+    assert len(docs) == 2
+
+
+def test_retrieve_invoke_args(api_token: str, org_id: str, pipeline_id: str) -> None:
+    retriever = VectorizeRetriever(api_token=api_token)
+    docs = retriever.invoke(
+        input="What are you?",
+        organization=org_id,
+        pipeline_id=pipeline_id,
+        num_results=2,
+    )
+    assert len(docs) == 2
